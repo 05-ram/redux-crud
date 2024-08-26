a9f22516c19f7f39702ada2a54ac739890bf9b5c
05-ram <karthik98@gmail.com>
2024-08-26 16:40:50 +0530

    dependencies with routing updated

---
+++ src/components/Users.js
@@ -0,0 +1,28 @@
+const user = [
+    {
+        id: "1",
+        name: 'Karthik',
+        skill: 'Full stack Developer',
+        mobile: "123456"
+    },
+    {
+        id: "2",
+        name: 'Ram',
+        skill: 'Front End Developer',
+        mobile: "654123"
+    },
+    {
+        id: "3",
+        name: 'Sam',
+        skill: 'UX Designer',
+        mobile: "987456321"
+    },
+    {
+        id: "4",
+        name: 'Tom',
+        skill: 'Back End Developer',
+        mobile: "852963"
+    }
+]
+
+export default user;

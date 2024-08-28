a507d141fd9a04800461f3d28fbb63b7e2bd861e
05-ram <karthik98@gmail.com>
2024-08-28 09:32:56 +0530

    delete functionality added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import users from "./Users";
+import React, { useState } from 'react';
+import initialUsers from './Users';
 import { Button, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 const Home = () => {
+    const [users, setUsers] = useState(initialUsers);
+
+    const handleDelete = (id) => {
+        setUsers(users.filter(user => user.id !== id));
+    }
+
     return (
         <>
             <div className='glbl-tbl'>
@@ -13,29 +20,44 @@ const Home = () => {
                             <th>Name</th>
                             <th>Skill</th>
                             <th>Mobile</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             users.map((user) => (
-                                <tr>
+                                <tr key={user.id}>
                                     <td>{user.id}</td>
                                     <td>{user.name}</td>
                                     <td>{user.skill}</td>
                                     <td>{user.mobile}</td>
+                                    <td>
+                                        <div className='d-flex gap-3 justify-content-center'>
+                                            <Link to={`/edit/${user.id}`}>
+                                                <Button variant='secondary'>Edit</Button>
+                                            </Link>
+                                            <Button 
+                                                variant='danger' 
+                                                className='ml-2' 
+                                                onClick={() => handleDelete(user.id)}
+                                            >
+                                                Delete
+                                            </Button>
+                                        </div>
+                                    </td>
                                 </tr>
                             ))
                         }
                     </tbody>
                 </Table>
                 <div className='d-flex justify-content-end'>
-                    <Link to={"/create"} >
+                    <Link to={"/create"}>
                         <Button variant='success'>Create</Button>
                     </Link>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Home
+export default Home;

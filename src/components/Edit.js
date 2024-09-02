1cabcb3a3febe52521c6824b46c1f7c50cc502f6
05-ram <karthik98@gmail.com>
2024-09-02 13:52:12 +0530

    changed crud methods

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,40 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
+import users from './Users';
 
 const Edit = () => {
-    const [name, setName] = useState('');
-    const [skill, setSkill] = useState('');
-    const [mobile, setMobile] = useState('');
-    const [id, setId] = useState('');
-    const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [skill, setSkill] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [id, setId] = useState('');
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setName(localStorage.getItem('name'));
-        setSkill(localStorage.getItem('skill'));
-        setMobile(localStorage.getItem('mobile'));
-    }, [])
+  let index = users
+    .map(function (e) {
+      return e.id;
+    }).indexOf(id)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        localStorage.setItem("name", name);
-        localStorage.setItem("skill", skill);
-        localStorage.setItem("mobile", mobile);
-        navigate('/');
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (name == "" || skill == "" || mobile === '') {
+      alert("invalid input");
+      return;
     }
-    return (
-        <div className='create-form'>
-            <h4 className='text-center'>Edit Form</h4>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) => setName(e.target.value)} value={name} /><br /><br />
-                <input type="text" onChange={(e) => setSkill(e.target.value)} value={skill} /><br /><br />
-                <input type="number" onChange={(e) => setMobile(e.target.value)} value={mobile} />
-                <div className='d-flex justify-content-end mt-4'>
-                    <Button type='submit' variant='success'>Update</Button>
-                </div>
-            </form>
+
+    let a = users[index];
+    console.log(a)
+    a.name = name;
+    a.skill = skill;
+    a.mobile = mobile;
+    a.id = id;
+    navigate("/");
+  };
+  useEffect(() => {
+    setName(localStorage.getItem("name"));
+    setSkill(localStorage.getItem("skill"));
+    setMobile(localStorage.getItem("mobile"));
+    setId(localStorage.getItem("id"));
+  }, []);
+
+  return (
+    <div className='create-form'>
+      <h4 className='text-center'>Edit Form</h4>
+      <form onSubmit={handelSubmit}>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} /><br /><br />
+        <input type="text" onChange={(e) => setSkill(e.target.value)} value={skill} /><br /><br />
+        <input type="number" onChange={(e) => setMobile(e.target.value)} value={mobile} />
+        <div className='d-flex justify-content-end mt-4'>
+          <Button type='submit' variant='success'>Update</Button>
         </div>
-    )
+      </form>
+    </div>
+  )
 }
 
 export default Edit
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,70 +1,75 @@
-import React, { useState } from 'react';
-import initialUsers from './Users';
+import React, { useEffect, useState } from 'react';
+import users from './Users';
 import { Button, Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const [users, setUsers] = useState(initialUsers);
+  let history = useNavigate();
 
-    const setValue = (id, name, skill, mobile) => {
-        localStorage.setItem("id", id);
-        localStorage.setItem("name", name);
-        localStorage.setItem("skill", skill);
-        localStorage.setItem("mobile", mobile);
-    }
+  const setValue = (id, name, skill, mobile) => {
+    localStorage.setItem("id", id);
+    localStorage.setItem("name", name);
+    localStorage.setItem("skill", skill);
+    localStorage.setItem("mobile", mobile);
+  }
 
-    const handleDelete = (id) => {
-        setUsers(users.filter(user => user.id !== id));
-    }
+  const handleDelete = (id) => {
+    let index = users
+      .map(function (e) {
+        return e.id;
+      }).indexOf(id)
+    users.splice(index, 1);
+    history('/')
+  }
 
-    return (
-        <>
-            <div className='glbl-tbl'>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Skill</th>
-                            <th>Mobile</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody> 
-                        {
-                            users.map((user) => (
-                                <tr key={user.id}>
-                                    <td>{user.id}</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.skill}</td>
-                                    <td>{user.mobile}</td>
-                                    <td>
-                                        <div className='d-flex gap-3 justify-content-center'>
-                                            <Link to={`/edit`}>
-                                                <Button variant='secondary' onClick={() => setValue(user.id, user.name, user.skill, user.mobile)}>Edit</Button>
-                                            </Link>
-                                            <Button
-                                                variant='danger'
-                                                className='ml-2'
-                                                onClick={() => handleDelete(user.id)}
-                                            >
-                                                Delete
-                                            </Button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </Table>
-                <div className='d-flex justify-content-end'>
-                    <Link to={"/create"}>
-                        <Button variant='success'>Create</Button>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className='glbl-tbl'>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Skill</th>
+              <th>Mobile</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.skill}</td>
+                  <td>{user.mobile}</td>
+                  <td>
+                    <div className='d-flex gap-3 justify-content-center'>
+                      <Link to={`/edit`}>
+                        <Button variant='secondary' onClick={() => setValue(user.id, user.name, user.skill, user.mobile)}>Edit</Button>
+                      </Link>
+                      <Button
+                        variant='danger'
+                        className='ml-2'
+                        onClick={() => handleDelete(user.id)}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
+        <div className='d-flex justify-content-end'>
+          <Link to={"/create"}>
+            <Button variant='success'>Create</Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
